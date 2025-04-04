6350ed2bf8cf693f595bf8fa5fec0feb12c0c488
predikdev <social.predik@gmail.com>
2025-04-04 19:17:18 +0200

    feat: User data update integration
    Added update form to change some user data
    
    Corresponding page (profile) also added
    
    Created /api/auth/update file

---
+++ src/components/auth/update-form.tsx
@@ -0,0 +1,88 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+import { useEffect, useState } from 'react';
+
+type Props = {
+  session: any;
+  userData: {
+    email?: string;
+    first_name?: string;
+    last_name?: string;
+    display_name?: string;
+  };
+};
+
+export function UpdateForm({
+  session,
+  userData,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & Props) {
+  return (
+    <div className={cn('flex flex-col gap-6 min-w-md', className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className='text-2xl'>
+            Update for: {userData.display_name}
+          </CardTitle>
+          <CardDescription>Use this form to update your data</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action='/api/auth/update' method='post'>
+            <div className='flex flex-col gap-6'>
+              <div className='grid gap-2'>
+                <Label htmlFor='display-name'>Display name</Label>
+                <Input
+                  id='display_name'
+                  type='text'
+                  name='display_name'
+                  defaultValue={userData.display_name}
+                />
+              </div>
+              <div className='grid gap-2'>
+                <Label htmlFor='first_name'>First name</Label>
+                <Input
+                  id='first_name'
+                  type='text'
+                  name='first_name'
+                  defaultValue={userData.first_name}
+                />
+              </div>
+              <div className='grid gap-2'>
+                <Label htmlFor='last_name'>Last name</Label>
+                <Input
+                  id='last_name'
+                  type='text'
+                  name='last_name'
+                  defaultValue={userData.last_name}
+                />
+              </div>
+              <div className='grid gap-2'>
+                <Label htmlFor='email'>Email</Label>
+                <Input
+                  id='email'
+                  type='email'
+                  name='email'
+                  defaultValue={userData.email}
+                />
+              </div>
+              <Button type='submit' className='w-full'>
+                Update
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ src/pages/api/auth/update.ts
@@ -0,0 +1,60 @@
+// With `output: 'static'` configured:
+export const prerender = false;
+import type { APIRoute } from 'astro';
+import { supabase } from '../../../lib/supabase';
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const first_name = formData.get('first_name')?.toString();
+  const last_name = formData.get('last_name')?.toString();
+  const display_name = formData.get('display_name')?.toString();
+  const email = formData.get('email')?.toString();
+
+  // Get current user ID from auth.users
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+
+  // Check for user
+  const { data: existingUser } = await supabase
+    .from('users')
+    .select()
+    .eq('id', user.id)
+    .single();
+
+  let error;
+
+  if (!existingUser) {
+    const { error: insertError } = await supabase.from('users').insert({
+      id: user.id,
+      first_name,
+      last_name,
+      display_name,
+      email,
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    });
+    error = insertError;
+  } else {
+    const { error: updateError } = await supabase
+      .from('users')
+      .update({
+        first_name,
+        last_name,
+        display_name,
+        email,
+        updated_at: new Date().toISOString(),
+      })
+      .eq('id', user.id);
+    error = updateError;
+  }
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
+  return redirect('/profile');
+};

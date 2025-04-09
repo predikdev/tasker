a0028b3a7d203eb470c4ab334031341989914682
predikdev <social.predik@gmail.com>
2025-04-10 00:38:28 +0200

    refactor: Redirect repaired to /login

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,9 +1,11 @@
 // With `output: 'static'` configured:
 export const prerender = false;
+import { supabase } from '@/lib/supabase';
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
+  await supabase.auth.signOut();
   cookies.delete('sb-access-token', { path: '/' });
   cookies.delete('sb-refresh-token', { path: '/' });
-  return redirect('/logout');
+  return redirect('/login');
 };
